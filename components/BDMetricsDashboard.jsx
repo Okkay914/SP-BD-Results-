d8988fd203f578c607f17e202203dc4e4c483842
Okkay914 <williambaumgarth@Gmail.com>
2024-11-22 15:07:03 -0500

    Update BDMetricsDashboard.jsx

--- components/BDMetricsDashboard.jsx
+++ components/BDMetricsDashboard.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Card, CardHeader, CardTitle, CardContent } from '../components/ui/card';
+import { Card, CardHeader, CardTitle, CardContent } from './ui/card';
 import { LineChart, Line, BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer, ReferenceLine } from 'recharts';
 
 const BDMetricsDashboard = () => {
