b79273a07d2cb8d6fa07794fde59704b985d8c4c
Okkay914 <williambaumgarth@Gmail.com>
2024-11-22 14:50:41 -0500

    Create page.js

---
+++ app/page.js
@@ -0,0 +1,9 @@
+import BDMetricsDashboard from '../components/BDMetricsDashboard'
+
+export default function Home() {
+  return (
+    <main className="min-h-screen p-4">
+      <BDMetricsDashboard />
+    </main>
+  )
+}
